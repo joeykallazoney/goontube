7d6b6d4209cac29b67bcfa0814e83929670876d7
James Skarzinskas <james@jskarzin.org>
2015-12-28 19:52:53 -0500

    test stub, basic koa setup, babelrc for es6

--- server.js
+++ server.js
@@ -2,3 +2,19 @@ import http from 'http'
 import koa from 'koa'
 import { Server as WebSocketServer } from 'ws'
 import { createStore } from 'redux'
+
+let server  = http.createServer()
+let app     = koa()
+let wss     = new WebSocketServer({ server: server })
+
+wss.on('connection', (ws) => {
+    console.log('New WebSocket connection.')
+})
+
+app.use(function *(next) {
+    this.status = 200
+    this.body = 'Goontube will be here.'
+})
+
+server.on('request', app.callback())
+server.listen(8080, () => console.log('Goontube listening on port 8080.'))
