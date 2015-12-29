29c8d8894f2ad9bb62d4777dea2f7c771831560b
James Skarzinskas <james@jskarzin.org>
2015-12-28 20:14:22 -0500

    remove protocol.js, will be in reducers

--- reducers.js
+++ reducers.js
@@ -3,4 +3,14 @@
  * an immutable state tree.
  * @since 1.0.0
  */
+import I from 'immutable'
 import { applyMiddleware, compose } from 'redux'
+
+const defaultApplicationState = I.Map({
+    media: {
+        id:         null,
+        provider:   null,
+        position:   0
+    },
+    playlist: []
+})
