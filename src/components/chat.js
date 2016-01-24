2b85bcadd35e8cbea2964bd9f1ff64a0668c0101
James Skarzinskas <james@jskarzin.org>
2016-01-24 18:36:11 -0500

    pure component refactor

--- src/components/chat.js
+++ src/components/chat.js
@@ -3,16 +3,47 @@ import ReactDOM from 'react-dom'
 
 import Users from './users'
 
+const KEYCODE_ENTER = 13
+
 let ChatMessage = (props) =>
     <li className="message">
         <span className="from">{props.from}</span>
         <div className="body">{props.body}</div>
     </li>
 
-let Input = (props) =>
-    <div className="chat-input">
-        <input type="text" />
+let ChatHistory = (props) =>
+    <div className="chat-list">
+    {
+        props.history.map((message) =>
+            <ChatMessage key={message.id} from={message.from} body={message.body} />
+        )
+    }
     </div>
+
+class ChatInput extends React.Component {
+    constructor(props, context) {
+        super(props, context)
+    }
+
+    onKeyDown(event) {
+        let nativeEvent = event.nativeEvent
+
+        if(KEYCODE_ENTER === nativeEvent.keyCode) {
+            let chatInputValue = this.refs.chatInput.value
+
+            console.log(chatInputValue)
+            this.refs.chatInput.value = ''
+        }
+    }
+
+    render() {
+        return (
+            <div className="chat-input">
+                <input ref="chatInput" type="text" onKeyDown={(e) => this.onKeyDown(e)} />
+            </div>
+        )
+    }
+}
 
 class Chat extends React.Component {
     constructor(props, context) {
@@ -40,16 +71,9 @@ class Chat extends React.Component {
 
         return (
             <div className="chat">
-                <ul className="chat-list">
-                {
-                    history.map((message) =>
-                        <ChatMessage key={message.id} from={message.from} body={message.body} />
-                    )
-                }
-                </ul>
-
+                <ChatHistory history={history} />
+                <ChatInput socket={this.props.socket} />
                 <Users />
-                <Input socket={this.props.socket} />
             </div>
         )
     }
