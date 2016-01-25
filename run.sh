ee17fec2a10f9f25d9022fe6f5e00fa37ae2ae00
Aria Bennett <twoplustwo@gmail.com>
2016-01-25 14:08:02 +0900

    added -browser flag to run script

--- run.sh
+++ run.sh
@@ -2,32 +2,40 @@
 
 # Script for consolidating the process of building and running the local goontube instance.
 
-while getopts ah opts; do
+while getopts ahs opts; do
    case ${opts} in
-      # -a (all): runs all processes required to build goontube. (does not currently install required shell binaries however.) 
       a)
-      sudo npm install
-      ;;
-      # -h (help): displays script summary and available flags.
+        sudo npm install
+        ;;
       h)
-      echo
-      echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
-      echo Script for consolidating the process of building and running the local goontube instance.
-      echo
-      echo
-      echo Flags:
-      echo __________________________________________________________________________________________
-      echo 
-      echo -a \(all\): runs all processes required to build goontube. \(Incomplete\)
-      echo -h \(help\): displays this, but you knew that already, didn\'t you?
-      echo __________________________________________________________________________________________
-      echo
-      echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
-      echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 
-      echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \(:V\)
-      echo                                                                                         
-      exit
-      ;;
+        echo
+        echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+        echo Script for consolidating the process of building and running the local goontube instance.
+        echo
+        echo
+        echo Flags:
+        echo __________________________________________________________________________________________
+        echo 
+        echo -a \(all\): runs all processes required to build goontube. \(Incomplete\)
+        echo -h \(help\): displays this, but you knew that already, didn\'t you?
+        echo -b \(browser\): runs the normal build and also switches to chrome. Requires OSX.
+        echo __________________________________________________________________________________________
+        echo
+        echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+        echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 
+        echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \(:V\)
+        echo                                                                                         
+        exit
+        ;;
+      b)
+        osascript -e '
+          tell application "Google Chrome"
+            open location "http://localhost:7070/"
+            delay 1
+            activate
+          end tell
+        '
+        ;;
    esac
 done
 
