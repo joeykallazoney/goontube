47dde2e46d607cdb41410320a8455970ccd4ee1c
Aria Bennett <twoplustwo@gmail.com>
2016-01-25 17:24:14 +0900

    c/p from comment in code: // If you're wondering why the line below is here, so am I! // npm-module youtube-node's subdependencies were // throwing errors with webpack for some reason. // "screendriver"'s post here was the only thing // that worked. Something something webpack needs help with json. // https://github.com/request/request/issues/1529 // // I don't care. // Thing works.

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,25 @@ module.exports = {
     module: {
         loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
+            // If you're wondering why the line below is here, so am I!
+            // npm-module youtube-node's subdependencies were
+            // throwing errors with webpack for some reason.
+            // "screendriver"'s post here was the only thing
+            // that worked. Something something webpack needs help with json.
+            // https://github.com/request/request/issues/1529
+            //
+            // I don't care.
+            // Thing works.
+            { test: /\.json$/, loader: 'json-loader' },
             { test: /\.sass$/, loader: ExtractTextPlugin.extract('style', 'css!sass?indentedSyntax') }
         ]
+    },
+    node: {
+      console: 'empty',
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
     },
     output: {
         filename: 'bundle.js',
