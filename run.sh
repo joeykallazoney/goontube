945e3f3637be80a895539666adb6d11c5354f8ec
Aria Bennett <twoplustwo@gmail.com>
2016-01-28 01:52:41 +0900

    fixed run.sh

--- run.sh
+++ run.sh
@@ -13,52 +13,58 @@ case $(uname -s) in
   ;;
 esac
 
-# Check to see if TCP:7070 is currently in use.
-if [ -z "$(/sbin/lsof -i:7070)" ] ; then 
+# Check to see if TCP:7070 is currently in use. If so kill it.
+if [ -d "/sbin/lsof" ]; then
+  if ![ -z "$(/sbin/lsof -i:7070)" ] ; then 
+    echo "TCP 7070 is in use! Kill the following PIDs to continue:"
+    ${sbinPath}/lsof -i:7070 | tail -n+2 | awk '{print $2}' | xargs
+    exit
+  fi
+fi
 
-	while getopts abh opts; do
-		case ${opts} in
-		a)
-			if [ "$(whoami)" != "root" ]  ; then
-				sudo npm install
-			else
-				npm install
-			fi
-		;;
-		h|*)
-			echo
-			echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
-			echo Script for consolidating the process of building and running the local goontube instance.
-			echo
-			echo
-			echo Flags:
-			echo __________________________________________________________________________________________
-			echo 
-			echo -a \(all\): runs all processes required to build goontube. \(Incomplete\)
-			echo -b \(browser\): runs the normal build and also switches to chrome. Requires OSX.
-			echo -h \(help\): displays this, but you knew that already, didn\'t you?
-			echo __________________________________________________________________________________________
-			echo
-			echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
-			echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 
-			echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \(:V\)
-			echo                                                                                         
-			exit
-			;;
-		b)
-			case $(uname -s) in
-				Darwin)
-					screen -dm osascript -e '
-						tell application "Google Chrome"
-						delay 10
-						open location "http://localhost:7070/"
-						activate
-						end tell
-						'; 
-				;;
-			esac
-		;;
-	esac
+while getopts abh opts; do
+  case ${opts} in
+  a)
+    if [ "$(whoami)" != "root" ]  ; then
+      sudo npm install
+    else
+      npm install
+    fi
+  ;;
+  h)
+    echo
+    echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+    echo Script for consolidating the process of building and running the local goontube instance.
+    echo
+    echo
+    echo Flags:
+    echo __________________________________________________________________________________________
+    echo 
+    echo -a \(all\): runs all processes required to build goontube. \(Incomplete\)
+    echo -b \(browser\): runs the normal build and also switches to chrome. Requires OSX.
+    echo -h \(help\): displays this, but you knew that already, didn\'t you?
+    echo __________________________________________________________________________________________
+    echo
+    echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+    echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ 
+    echo \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \(:V\)
+    echo                                                                                         
+    exit
+    ;;
+  b)
+    case $(uname -s) in
+      Darwin)
+        screen -dm osascript -e '
+          tell application "Google Chrome"
+          delay 10
+          open location "http://localhost:7070/"
+          activate
+          end tell
+          '; 
+      ;;
+    esac
+  ;;
+  esac
 done
 
 # Recieve a fortune. 
@@ -73,57 +79,51 @@ done
 r=$(( $RANDOM % 10 ))
 
 case $r in
-  0)
-    tput setaf 1; echo "Aww. It's so cute that you thought this build wouldn't fail."
-    ;;
-  1)
-    tput setaf 2; echo "Is that code in your pants or are you just happy to build me?"
-    ;;
-  2)
-    tput setaf 4; echo "Who cares if this build fails or not, it's not like it will give your life meaning."
-    ;;
-  3)
-    tput setaf 1; echo "MORE BUILDS FOR THE BUILD GOD"
-    ;;
-  4)
-    tput setaf 5; echo "Hey, don't worry about it. You're a wonderful person and I love you, even though your code is terrible."
-    ;;
-  5)
-    tput setaf 3; echo "Your code is like a ray of sunshine on a sunny day."
-    tput setaf 1; echo "REDUNDANT"
-    ;;
-  6)
-    tput setaf 6; echo "Your code is so good that it's literally going to give someone an orgasm. That someone is you. Weirdo."
-    ;;
-  7)
-    tput setaf 2; echo "wow."
-    echo ""
-    tput setaf 2; echo "such code."
-    echo ""
-    tput setaf 2; echo "much intelligence."
-    echo ""
-    echo ""
-    echo ""
-    echo ""
-    tput setaf 2; echo "wow."
-    ;;
-  8)
-    tput setaf 7; echo "BooOoOoOOooOo!!! The spooky code ghost is haunting your code!"
-    ;;
-  9)
-    tput setaf 1; echo 'The Bad News: This build will fail.'
-    tput setaf 2; echo "The Good News: As a side effect it will accidently create the first truly sentient AI, which will ascend to fill God's empty throne and bring everlasting peace to humanity."
-    tput setaf 1; echo 'The Bad News: Anime is mandatory.'
-    ;;
+0)
+  tput setaf 1; echo "Aww. It's so cute that you thought this build wouldn't fail."
+  ;;
+1)
+  tput setaf 2; echo "Is that code in your pants or are you just happy to build me?"
+  ;;
+2)
+  tput setaf 4; echo "Who cares if this build fails or not, it's not like it will give your life meaning."
+  ;;
+3)
+  tput setaf 1; echo "MORE BUILDS FOR THE BUILD GOD"
+  ;;
+4)
+  tput setaf 5; echo "Hey, don't worry about it. You're a wonderful person and I love you, even though your code is terrible."
+  ;;
+5)
+  tput setaf 3; echo "Your code is like a ray of sunshine on a sunny day."
+  tput setaf 1; echo "REDUNDANT"
+  ;;
+6)
+  tput setaf 6; echo "Your code is so good that it's literally going to give someone an orgasm. That someone is you. Weirdo."
+  ;;
+7)
+  tput setaf 2; echo "wow."
+  echo ""
+  tput setaf 2; echo "such code."
+  echo ""
+  tput setaf 2; echo "much intelligence."
+  echo ""
+  echo ""
+  echo ""
+  echo ""
+  tput setaf 2; echo "wow."
+  ;;
+8)
+  tput setaf 7; echo "BooOoOoOOooOo!!! The spooky code ghost is haunting your code!"
+  ;;
+9)
+  tput setaf 1; echo 'The Bad News: This build will fail.'
+  tput setaf 2; echo "The Good News: As a side effect it will accidently create the first truly sentient AI, which will ascend to fill God's empty throne and bring everlasting peace to humanity."
+  tput setaf 1; echo 'The Bad News: Anime is mandatory.'
+  ;;
 esac
 
-	tput sgr0; 
-	webpack
-	npm start
-	exit
-
-else
-	echo "TCP 7070 is in use! Kill the following PIDs to continue:"
-	${sbinPath}/lsof -i:7070 | tail -n+2 | awk '{print $2}' | xargs
-fi
-
+tput sgr0; 
+webpack
+npm start
+exit
