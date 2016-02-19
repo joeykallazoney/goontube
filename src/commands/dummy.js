d5b2a8e3c5f5a12a387889e3fbf0a2d5593fc649
James Skarzinskas <james@jskarzin.org>
2016-02-18 23:13:58 -0500

    remove  login

--- src/commands/dummy.js
+++ src/commands/dummy.js
@@ -31,6 +31,5 @@ export default {
      * message which is considered to have passed and is being parsed.
      */
     commandHandler: (server, client, message) => {
-        client.login(message, 'xtremepassword12345')
     }
 }
