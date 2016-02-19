42c07298e831ed7ca6454fa0074cfa55f7dd050c
James Skarzinskas <james@jskarzin.org>
2016-02-19 00:18:52 -0500

    fix hash compose

--- shared/hash.js
+++ shared/hash.js
@@ -1,5 +1,4 @@
 import { SHA512, enc } from 'crypto-js'
 
-export default (salt, inputString) =>
-    salt => inputString =>
-        SHA512(`{${inputString}${salt}}`).toString(enc.Hex)(inputString)
+export default salt => inputString =>
+    SHA512(`${salt + inputString}`).toString(enc.Hex)
