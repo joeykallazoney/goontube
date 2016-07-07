9530c5c2b19c222e70e4689b43df2d8baed109ea
James Skarzinskas <james@jskarzin.org>
2016-07-07 12:14:28 -0400

    correct for redux-saga API change

--- client/middleware/index.js
+++ client/middleware/index.js
@@ -1,15 +1,12 @@
 import { applyMiddleware, compose } from 'redux'
-import sagaMiddleware from 'redux-saga'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 import invariant from 'redux-immutable-state-invariant'
 import webSocketMiddleware from './websocket'
-import rootSaga from '../sagas'
 
 export default applyMiddleware(
     invariant(),
     multi,
     thunk,
-    webSocketMiddleware,
-    sagaMiddleware(rootSaga)
+    webSocketMiddleware
 )
