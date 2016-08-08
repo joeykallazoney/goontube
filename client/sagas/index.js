a027d9a149a802f6ee9c05017522e43556027180
J. Eric Stout <jericstout@gmail.com>
2016-08-08 12:29:40 -0500

    Fork and Clean
    Goontube loves your mommas flapjacks.

--- client/sagas/index.js
+++
@@ -1,6 +0,0 @@
-import { fork, call } from 'redux-saga/effects'
-import validationSaga from './validation'
-
-export default function* rootSaga() {
-    yield fork(validationSaga)
-}
--- shared/hash.js
+++
@@ -1,4 +0,0 @@
-import { SHA512, enc } from 'crypto-js'
-
-export default salt => inputString =>
-    SHA512(`${salt + inputString}`).toString(enc.Hex)
